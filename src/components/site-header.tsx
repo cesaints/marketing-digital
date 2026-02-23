562ce10ce3e886c336e6c5a161cb2d7a23d29f91
Carlos Eduardo <cesaints.engineer@gmail.com>
2026-02-23 19:40:22 +0000

    feat: remove public signup entrypoints

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -16,12 +16,6 @@ export function SiteHeader() {
           >
             Entrar
           </Link>
-          <Link
-            href="/cadastro"
-            className="rounded-md px-3 py-2 text-sm hover:bg-slate-100 dark:hover:bg-slate-900"
-          >
-            Criar conta
-          </Link>
 
           <ThemeToggle />
         </nav>
